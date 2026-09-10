a37728fceffe6e8df52ad5470009631dd2623571
kubahemascv-cpu <kubahemascv@gmail.com>
2026-09-10 16:12:42 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // File statis, permanen di repo. Bikin Android bisa mengenali app ini
 // sebagai PWA yang bisa diinstall permanen (bukan sekadar shortcut).
 
-const CACHE = 'cs-toko-jobdesk-v7';
+const CACHE = 'cs-toko-jobdesk-v8';
 
 self.addEventListener('install', function (e) {
   e.waitUntil(
